a8aa4e3e4ff6b414d8081e280d788fd8c7e11bff
stevie2codes <stevie2codes@gmail.com>
2020-02-08 10:46:34 -0600

    MVC
    Setup MVC Files along with server and DB

---
+++ server.js
@@ -0,0 +1,27 @@
+let express = require("express");
+const db = require("./models");
+
+let PORT = process.env.PORT || 8080;
+
+let app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+require("./controllers/nutriController")(app);
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);  });
+});
