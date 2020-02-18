343edfae2603a0ca5b0a4a401b2c16321951dffe
stevie2codes <stevie2codes@gmail.com>
2020-02-18 12:49:13 -0600

    Recipe Post
    Hooked in recipe data fields and saving them to DB also created new handlebar page to display data with styles

--- public/assets/js/create.js
+++ public/assets/js/create.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
-
+    //Get field inputs and save them to db with click of submit recipe
     let recipeName = $("#recipeName");
     let ingredientValue = $("#iInput");
-    let directionsValue = $("$dInput");
+    let directionsValue = $("#dInput");
     let submitRecipe = $("#submitRecipe");
     let selectUser = $("#userSelect");
 
-})
+
+
+
+    submitRecipe.on("click", event => {
+        event.preventDefault();
+        let newRecipe = {
+            recipeName: recipeName.val().trim(),
+            ingredients: ingredientValue.val().trim(),
+            directions: directionsValue.val().trim()
+        };
+        console.log(newRecipe)
+        $.ajax("/api/newRecipe", {
+            type: "POST",
+            data: newRecipe
+        }).then(data => {
+            console.log(data);
+            window.location.href = "/api/newRecipe";
+        });
+    });
+
+});
